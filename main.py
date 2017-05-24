0de7ddb02d5308ddb1ca31d2cd792fda43dafcc7
Kavi Gupta <kavi@globalmbox.com>
2017-05-23 20:36:03 -0700

    Finish export

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ if not os.path.exists("outputs"):
     os.makedirs("outputs")
 
 for cutoff_perc in 1, 50, 90, 95, 99, 99.5, 99.9, 100:
-    create_vote_density_map("2016-results.csv", cutoff_perc, "outputs/colorized-election-%03.2f.svg" % cutoff_perc)
+    create_vote_density_map("2016-results.csv", cutoff_perc, "outputs/colorized-election-%06.2f.svg" % cutoff_perc)
